7eef1218de202c2b22e6fb62a102a0453e4be9b3
Junha Park <tunatone.0111@gmail.com>
2022-01-31 05:00:26 +0900

    [feat] add user entity class

--- src/routes/user/entities/user.entity.ts
+++ src/routes/user/entities/user.entity.ts
@@ -1,5 +1,9 @@
-import { Role } from "@prisma/client";
+import { Role, User } from "@prisma/client";
 import { Static, Type } from "@sinclair/typebox";
+
+type UserExtended = User & {
+  department: { name: string; college: { name: string } };
+};
 
 export const UserEntitySchema = Type.Object({
   id: Type.Integer(),
@@ -13,4 +17,26 @@ export const UserEntitySchema = Type.Object({
   isAdmin: Type.Boolean(),
 });
 
-export type UserEntity = Static<typeof UserEntitySchema>;
+export class UserEntity implements Static<typeof UserEntitySchema> {
+  id: number;
+  academicId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  department: string;
+  college: string;
+  role: Role;
+  isAdmin: boolean;
+
+  constructor(user: UserExtended) {
+    this.id = user.id;
+    this.academicId = user.academicId;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.email = user.email;
+    this.department = user.department.name;
+    this.college = user.department.college.name;
+    this.role = user.role;
+    this.isAdmin = user.isAdmin;
+  }
+}
