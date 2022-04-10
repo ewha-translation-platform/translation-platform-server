2852cf2313b303e3ab8a2fab7272bea118fc75e0
Junha Park <tunatone.0111@gmail.com>
2022-04-11 06:34:20 +0900

    [fix] fix account register

--- src/routes/user/dto/update-user.dto.ts
+++ src/routes/user/dto/update-user.dto.ts
@@ -1,7 +1,13 @@
 import { Static, Type } from "@sinclair/typebox";
 import { CreateUserDtoSchema } from "./create-user.dto";
 
-export const UpdateUserDtoSchema = Type.Partial(
-  Type.Omit(CreateUserDtoSchema, ["id"])
-);
+export const UpdateUserDtoSchema = Type.Intersect([
+  Type.Partial(CreateUserDtoSchema),
+  Type.Partial(
+    Type.Object({
+      academicId: Type.Union([Type.Null(), Type.String()]),
+    })
+  ),
+]);
+
 export type UpdateUserDto = Static<typeof UpdateUserDtoSchema>;
